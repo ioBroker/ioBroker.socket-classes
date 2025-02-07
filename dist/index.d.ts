d9d071e5bf5607c6b5b126b058a7862ddbb6aafb
GermanBluefox <dogafox@gmail.com>
2025-02-07 21:12:14 +0000

    chore: release v2.0.5
    * (@GermanBluefox) Code migrated to TypeScript

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,9 @@
 import passport from 'passport';
+export { type SocketSubscribeTypes } from './types';
 import cookieParser from 'cookie-parser';
 export { SocketCommandsAdmin } from './lib/socketCommandsAdmin';
 export { SocketCommands, type SupportedFeature } from './lib/socketCommands';
-export { SocketCommon, type Server } from './lib/socketCommon';
+export { SocketCommon, type Server, type SocketSettings, type SocketIoOptions } from './lib/socketCommon';
 export { SocketAdmin } from './lib/socketAdmin';
 import { authorize as passportSocket, type PassportHttpRequest, type Store } from './lib/passportSocket';
 export { passport, cookieParser, passportSocket, type PassportHttpRequest, type Store };
